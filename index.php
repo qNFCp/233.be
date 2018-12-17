9f2daa634f381369cf21044f3c21cac0c0b6124d
qNFCp <31499043+qNFCp@users.noreply.github.com>
2018-12-17 09:55:30 +0800

    Update index.php
    移动端自适应

--- index.php
+++ index.php
@@ -21,7 +21,8 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8" />   
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title><?php echo get_title() . ' - ' . get_description(); ?></title>
     <meta name="description" content="" />
